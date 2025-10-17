c92242a2853bd6f40ead44a2d2ab6eeee08ab5d6
nabilasaf <nabilasafitri861@gmail.com>
2025-10-17 19:33:20 +0700

    Tugas3

--- 20230140243-node-server/server.js
+++ 20230140243-node-server/server.js
@@ -1,24 +1,28 @@
- 	const express = require('express');
- 	const cors = require('cors'); 
- 	const app = express();
- 	const PORT = 3001;
- 	
- 	// Middleware
- 	app.use(cors()); 
- 	app.use(express.json()); 
- 	app.use((req, res, next) => {
- 	  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
- 	  next();
- 	});
- 	
-app.get('/', (req, res) => {
- res.send('Home Page for API');
- 	});
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
 
-const bookRoutes = require("./Routes/books");
-app.use('/api/books', bookRoutes);
-    
- 	
- 	app.listen(PORT, () => {
- 	  console.log(`Express server running at http://localhost:${PORT}/`);
- 	});
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
---
+++ Tugas3/Tugas3.md
@@ -0,0 +1,14 @@
+Tampilan Check in pertama :
+![Tampilan Check in](SS/CheckIn.png)
+
+Tampilan Check in kedua :
+![Tampilan Check in](SS/CheckIn2.png)
+
+Tampilan Check out pertama :
+![Tampilan Check out](SS/CheckOut.png)
+
+Tampilan Check out kedua :
+![Tampilan Check out](SS/CheckOut2.png)
+
+Tampilan Reports :
+![Tampilan Reports](SS/Reports.png)
